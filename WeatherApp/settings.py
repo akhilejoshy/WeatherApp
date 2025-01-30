eb19c4e3b5fbd7f666e5cbe8d0df83a5522bd6cb
akhilejoshy <akhilejoshy@gmail.com>
2025-01-30 19:41:00 +0530

    updated setings

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['https://weatherapp-akhiljoshy.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
