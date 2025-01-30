18b13d944a1e103eb9461083bde7375ed97d3609
akhilejoshy <akhilejoshy@gmail.com>
2025-01-30 20:08:14 +0530

    updated

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,8 +118,7 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
